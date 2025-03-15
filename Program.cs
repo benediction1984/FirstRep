9ce91e5edc45ae7046c4f004609933e4b93ef58b
John Paul Naranjo <clemency0916@yahoo.com>
2025-03-14 23:10:10 -0700

    Replaced default code with a new program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+//Challenge: "Guess the Number" Game
+Console.WriteLine("Computer: I am thinking of a number between 1 to 100. You have 5 chances to guess it right.");
 
-Console.WriteLine("Hello, World!");
+Random rdm = new Random();
+
+int computerNumber = rdm.Next(1, 101);
+int chances = 5;
+while (true)
+{
+    Console.Write("Enter the guessed number here: ");
+    string input = Console.ReadLine();
+     
+    if (int.TryParse(input, out int integerInput))
+    {
+        if (integerInput < 0)
+        {
+            Console.WriteLine("Number cannot be negative number. It must be between 1 to 100.");
+        }
+        else if (computerNumber == integerInput)
+        {
+            Console.WriteLine($"You guessed it right! The number is {computerNumber}");
+            break;
+        }
+        else if (integerInput > computerNumber)
+        {
+            chances--;
+            Console.WriteLine($"{integerInput} is too big. Try again. You have {chances} chances left");
+        }
+        else
+        {
+            chances--;
+            Console.WriteLine($"{integerInput} is too small. Try again. You have {chances} chances left");
+        }
+
+        if (chances == 0)
+        {
+            Console.WriteLine($"Game is over. You lose. The answer is {computerNumber}");
+            break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Please enter an integer.");
+    }
+    
+}
+
